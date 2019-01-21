e797064d4636e0fde3c8193180565151c39b83f5
Flavio Monteiro <flaviolas1@hotmail.com>
2019-01-21 16:38:14 +0000

    terceiro commit

--- sicNoticias.php
+++ sicNoticias.php
@@ -23,37 +23,37 @@ $dom = new DOMDocument();
 @$dom->loadHTML($html);
 
 $xpath = new DOMXpath($dom);
-$elements = $xpath->query("//article");
+$elements = $xpath->query("////div[contains(@class,'full-article')]");
 $descricaoDetalhadaQuery = $xpath->query("//div[contains(@class,'article-content')]");
 //sizearticle col-md-9 col-sm-12 pull-right
 $element = $elements->item(0);
 $titulo = $element->getElementsByTagName('h1')->item(0)->nodeValue;
 
-if ($element->getElementsByTagName('link')->item(0)!=null)
-{
+if($element->getElementsByTagName('video')->item(0)!=null){
 
-$img = $element->getElementsByTagName('link')->item(0)->getAttribute("href");
+    $img = $element->getElementsByTagName('video')->item(0)->getAttribute('poster');
 
-}
+}else if ($element->getElementsByTagName('img')->item(0)!=null){
 
-else if($element->getElementsByTagName('img')->item(0)!=null){
-
-    $img = $element->getElementsByTagName('img')->item(0)->getAttribute("src");
+    $img = $element->getElementsByTagName('img')->item(0)->getAttribute('src');
 
 }else{
     $img = "Sem Imagem" . PHP_EOL;
 
 }
-var_dump($img);
+
 //$descricaoPrincipal = $element->getElementsByTagName('h2')->item(0)->nodeValue;
-if ($descricaoDetalhadaQuery->length != 0) {
+if ($descricaoDetalhadaQuery->length != 0 && $descricaoDetalhadaQuery->item(0)->getElementsByTagName('p')->count()!=0) {
     $descricaoDetalhada = $descricaoDetalhadaQuery->item(0)->getElementsByTagName('p');
  $corpo = "";
+
     foreach ($descricaoDetalhada as $cenas) {
 
             $corpo .= $cenas->nodeValue;
 
         }
+}else{
+    $corpo = $element->getElementsByTagName('h2')->item(0)->nodeValue;
 }
 
         $mRet = array(
@@ -68,6 +68,6 @@ if ($descricaoDetalhadaQuery->length != 0) {
 
 $sicResult = new sic();
 
-$result = $sicResult->getNoticias("https://sicnoticias.sapo.pt/mundo/2018-03-14-Cientistas-alertam-para-riscos-eticos-de-uso-de-inteligencia-artificial-na-medicina");
+$result = $sicResult->getNoticias("https://sicnoticias.sapo.pt/desporto/2019-01-21-Sergio-Conceicao-espera-um-classico-dificil-com-o-Benfica");
 
 var_dump($result);
